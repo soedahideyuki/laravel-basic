2101df7bc1ef484c4731bd564c73021b9c4ab902
soedahideyuki <kiyudehi811@gmail.com>
2024-03-10 20:35:04 +0900

    ORMについて

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function index(){
         $products = DB::table('products')->get();
         return view('products.index', compact('products'));
+    }
+
+    public function show($id){
+        $product = Product::find($id);
+        return view('products.show', compact('product'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::get('/', function () {
 Route::get('/hello', [HelloController::class, 'index']);
 
 Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
