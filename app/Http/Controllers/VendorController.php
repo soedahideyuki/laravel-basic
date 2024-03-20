2bfc5e0d85f7ca9633f220e8de103c8ce46e33c1
soedahideyuki <kiyudehi811@gmail.com>
2024-03-20 15:58:54 +0900

    バリデーション未完成

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -15,4 +15,23 @@ class VendorController extends Controller
 
         return view('vendors.show', compact('vendor', 'products'));
     }
+
+    public function create(){
+        return view('vendors.create');
+    }
+
+    public function store(Request $request){
+        //バリデーしょんの設定
+        $request->validate([
+            'vendor_code' => 'required|inteder|min:0|unique:vendors,vendor_code',
+            'vender_name' => 'required|max:255'
+        ]);
+
+        $vendor = new Vendor();
+        $vendor -> vendor_code = $request->input('vendor_code');
+        $vendor -> vender_name = $request->input('vender_name');
+        $vendor -> save();
+
+        return redirect("/vendors/{$vendor->id}");
+    }
 }
---
+++ resources/views/vendors/create.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel基礎</title>
+</head>
+<body>
+  <h1>仕入れ先登録</h1>
+
+  @if($errors->any())
+  <div>
+    <ul>
+      @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
+  
+  <form action="{{ route('vendors.store') }}" method="POST">
+    @csrf
+    <table>
+      <tr>
+        <th>仕入れ先code</th>
+        <td>
+          <input type="number" name="vendor_code">
+        </td>
+      </tr>
+      <tr>
+        <th>仕入先名</th>
+        <td>
+          <input type="text" name="vender_name">
+        </td>
+      </tr>
+    </table>
+    <input type="submit" value="登録">
+  </form>
+</body>
+</html>
