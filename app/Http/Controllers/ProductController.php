2be6fb7d9aefa3afb72a56273b4e716db4ab67ce
soedahideyuki <kiyudehi811@gmail.com>
2024-03-20 18:11:34 +0900

    フォームリクエスト

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Product;
+use App\Models\Vendor;
+use App\Http\Requests\ProductStoreRequest;
 
 class ProductController extends Controller
 {
@@ -17,5 +19,21 @@ class ProductController extends Controller
     public function show($id){
         $product = Product::find($id);
         return view('products.show', compact('product'));
+    }
+
+    public function create(){
+        $vendor_codes = Vendor::pluck('vendor_code');
+
+        return view('products.create', compact('vendor_codes'));
+    }
+
+    public function store(ProductStoreRequest $request){
+        $product = new Product();
+        $product->product_name = $request->input('product_name');
+        $product->price = $request->input('price');
+        $product->vendor_code = $request->input('vendor_code');
+        $product->save();
+
+        return redirect("/products/{$product->id}");
     }
 }
