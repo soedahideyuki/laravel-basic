af1bbd686e6bdfcbbca17a046dfae502fdd50018
soedahideyuki <kiyudehi811@gmail.com>
2024-03-26 17:29:09 +0900

    session complete

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\RequestController;
 use App\Http\Controllers\ResponseController;
 use App\Http\Controllers\SignInController;
 use App\Http\Controllers\CookieController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -55,3 +56,8 @@ Route::get('/cookies', [CookieController::class, 'index']);
 Route::get('/cookies/create', [CookieController::class, 'create'])->name('cookies.create');
 Route::post('/cookies/store', [CookieController::class, 'store'])->name('cookies.store');
 Route::delete('/cookies/destroy', [CookieController::class, 'destroy'])->name('cookies.destroy');
+
+Route::get('/sessions', [SessionController::class, 'index']);
+Route::get('/sessions/create', [SessionController::class, 'create'])->name('sessions.create');
+Route::post('/sessions/store', [SessionController::class, 'store'])->name('sessions.store');
+Route::delete('/sessions/destroy', [SessionController::class, 'destroy'])->name('sessions.destroy');
